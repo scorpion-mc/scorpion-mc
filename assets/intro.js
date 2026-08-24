016d17b5d6200b5d40907d07a61811504a976614
Scorpion Admin <admin@scorpionmc.com>
2026-08-24 20:41:50 +0300

    Fill intro width and add flag wave sound

--- assets/intro.js
+++ assets/intro.js
@@ -3,10 +3,18 @@
   const intro=document.querySelector('.mc-intro');
   if(!intro)return;
   const video=intro.querySelector('.intro-video');
+  const flagAudio=intro.querySelector('.flag-wave-audio');
+  const soundButton=intro.querySelector('.intro-sound');
   let finished=false;
+  const startSound=()=>{
+    if(!flagAudio)return;
+    flagAudio.volume=.48;
+    flagAudio.play().then(()=>soundButton?.classList.add('is-on')).catch(()=>{});
+  };
   const finish=()=>{
     if(finished)return;
     finished=true;
+    if(flagAudio){flagAudio.pause();flagAudio.currentTime=0}
     intro.classList.add('is-leaving');
     document.body.classList.remove('intro-locked');
     const backgroundVideo=document.querySelector('.video-bg[data-src]');
@@ -22,5 +30,12 @@
   video.addEventListener('ended',finish,{once:true});
   video.addEventListener('error',finish,{once:true});
   video.play().catch(finish);
+  soundButton?.addEventListener('click',()=>{
+    video.currentTime=0;
+    video.play().catch(()=>{});
+    startSound();
+  });
+  ['pointerdown','keydown','touchstart'].forEach(eventName=>document.addEventListener(eventName,startSound,{once:true,passive:true}));
+  startSound();
   setTimeout(finish,7000);
 })();
